2cc98d1a57cb57577dd71245dac350a78b8828d0
Piotr Czarnecki <czarnecki.devv@gmail.com>
2025-07-12 20:01:14 +0200

    feature/create gui elements for ui functionality
    Create labels, texteditors and textbuttons that allows fundamental functionality of the plugin.
    
    Style GUI elements:
    - Create colour map that assigns RGB colours to the descriptive name
    - Set up fonts from BinaryData (meaning, from .ttf files)
    - Assign sizes to each element and add them to the UI

--- BeatUploader/Source/PluginEditor.cpp
+++ BeatUploader/Source/PluginEditor.cpp
@@ -4,19 +4,204 @@
 BeatUploaderAudioProcessorEditor::BeatUploaderAudioProcessorEditor (BeatUploaderAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    setSize (400, 300);
+    setSize (screenWidth, screenHeight);
+
+    // colours map that assigns RGB colour to descriptive name
+    colours["bg"] = juce::Colour(15, 15, 15);
+    colours["labelBg"] = juce::Colour(31, 31, 31);
+
+    colours["font"] = juce::Colour(223, 223, 223);
+    colours["info"] = juce::Colour(209, 209, 209);
+    colours["idle"] = juce::Colour(201, 201, 201);
+
+    colours["btnOnTxt"] = juce::Colour(217, 217, 217);
+    colours["btnOnBg"] = juce::Colour(26, 26, 26);
+
+    colours["hl"] = juce::Colour(92, 176, 255);
+    colours["hlTxt"] = juce::Colour(245, 245, 245);
+
+    colours["error"] = juce::Colour(230, 32, 32);
+
+    // fonts setup
+    titleTypeface = juce::Typeface::createSystemTypefaceFor(
+        BinaryData::MichromaRegular_ttf,
+        BinaryData::MichromaRegular_ttfSize
+    );
+    titleFont = juce::Font(titleTypeface).withHeight(42.0f);
+
+    infoTypeface = juce::Typeface::createSystemTypefaceFor(
+        BinaryData::MontserratLightItalic_ttf,
+        BinaryData::MontserratLightItalic_ttfSize
+    );
+    infoFont = juce::Font(infoTypeface).withHeight(19.0f);
+
+    boxTypeface = juce::Typeface::createSystemTypefaceFor(
+        BinaryData::MontserratSemiBold_ttf,
+        BinaryData::MontserratSemiBold_ttfSize
+    );
+    boxFont = juce::Font(boxTypeface).withHeight(23.0f);
+
+    buttonTypeface = juce::Typeface::createSystemTypefaceFor(
+        BinaryData::MontserratRegular_ttf,
+        BinaryData::MontserratRegular_ttfSize
+    );
+    buttonFont = juce::Font(buttonTypeface).withHeight(20.0f);
+
+
+    // makes visible and adds functionality to:
+    // logging button
+    addAndMakeVisible(loginBtn);
+    loginBtn.onClick = [this]() {
+        this->login();
+    };
+
+    // textboxes and labels
+    addAndMakeVisible(titleBox);
+    addAndMakeVisible(dscBox);
+
+    addAndMakeVisible(title);
+    addAndMakeVisible(info);
+    addAndMakeVisible(output);
+
+    // audio and image input
+    addAndMakeVisible(audioSelect);
+    audioSelect.onClick = [this]() {
+        /*juce::FileChooser chooser("Select an audio file", {}, "*.mp3;*.wav;*.flac;*.ogg");
+        audio = chooser.getResult();
+
+        if (!audio.existsAsFile()) {
+            output.setColour(juce::Label::textColourId, colours["error"]);
+            output.setText("Audio was not chosen", juce::dontSendNotification);
+        }
+        else audioChosen = true;*/
+    };
+
+    addAndMakeVisible(imageSelect);
+    imageSelect.onClick = [this]() {
+        /*juce::FileChooser chooser("Select an image file", {}, "*.jpg;*.png;*.jpeg;*.gif");
+        image = chooser.getResult();
+
+        if (!image.existsAsFile()) {
+            output.setColour(juce::Label::textColourId, colours["error"]);
+            output.setText("Image was not chosen", juce::dontSendNotification);
+        }
+        else imageChosen = true;*/
+    };
+
+    // upload button
+    addAndMakeVisible(uploadBtn);
+    uploadBtn.onClick = [this]() {
+        this->upload();
+    };
+
+
+    // set up font for TextButtons
+    lookAndFeel = std::make_unique<customTextButtonFont>(buttonFont);
+
+    loginBtn.setLookAndFeel(lookAndFeel.get());
+    audioSelect.setLookAndFeel(lookAndFeel.get());
+    imageSelect.setLookAndFeel(lookAndFeel.get());
+    uploadBtn.setLookAndFeel(lookAndFeel.get());
+}
+BeatUploaderAudioProcessorEditor::~BeatUploaderAudioProcessorEditor(){}
+
+void BeatUploaderAudioProcessorEditor::login()
+{
+    
 }
 
-BeatUploaderAudioProcessorEditor::~BeatUploaderAudioProcessorEditor()
+void BeatUploaderAudioProcessorEditor::upload()
 {
+
 }
 
 void BeatUploaderAudioProcessorEditor::paint (juce::Graphics& g)
 {
+    // sets colour, font and text for each gui element
+    // font for TextButtons is set up in constructor
+    g.fillAll(colours["bg"]);
 
+    title.setColour(juce::Label::textColourId, colours["font"]);
+    title.setColour(juce::Label::backgroundColourId, colours["bg"]);
+    title.setJustificationType(juce::Justification::centred);
+    title.setFont(titleFont);
+    title.setText("Beat Uploader", juce::dontSendNotification);
+
+    loginBtn.setColour(juce::TextButton::textColourOffId, colours["font"]);
+    loginBtn.setColour(juce::TextButton::textColourOnId, colours["btnOnTxt"]);
+    loginBtn.setColour(juce::TextButton::buttonColourId, colours["labelBg"]);
+    loginBtn.setColour(juce::TextButton::buttonOnColourId, colours["btnOnBg"]);
+    loginBtn.setButtonText("Login");
+
+    info.setColour(juce::Label::textColourId, colours["info"]);
+    info.setColour(juce::Label::backgroundColourId, colours["bg"]);
+    info.setFont(infoFont);
+    info.setText("Google account (YouTube channel)", juce::dontSendNotification);
+
+    titleBox.setColour(juce::TextEditor::textColourId, colours["font"]);
+    titleBox.setColour(juce::TextEditor::backgroundColourId, colours["labelBg"]);
+    titleBox.setColour(juce::TextEditor::highlightedTextColourId, colours["hlTxt"]);
+    titleBox.setColour(juce::TextEditor::highlightColourId, colours["hl"]);
+    titleBox.setFont(boxFont);
+    titleBox.setJustification(juce::Justification::centredLeft);
+    titleBox.setTextToShowWhenEmpty("Title...", colours["idle"]);
+
+    dscBox.setColour(juce::TextEditor::textColourId, colours["font"]);
+    dscBox.setColour(juce::TextEditor::backgroundColourId, colours["labelBg"]);
+    dscBox.setColour(juce::TextEditor::highlightedTextColourId, colours["hlTxt"]);
+    dscBox.setColour(juce::TextEditor::highlightColourId, colours["hl"]);
+    dscBox.setFont(boxFont);
+    dscBox.setTextToShowWhenEmpty("Description...", colours["idle"]);
+
+    audioSelect.setColour(juce::TextButton::textColourOffId, colours["font"]);
+    audioSelect.setColour(juce::TextButton::textColourOnId, colours["btnOnTxt"]);
+    audioSelect.setColour(juce::TextButton::buttonColourId, colours["labelBg"]);
+    audioSelect.setColour(juce::TextButton::buttonOnColourId, colours["btnOnBg"]);
+    audioSelect.setButtonText("Audio");
+
+    imageSelect.setColour(juce::TextButton::textColourOffId, colours["font"]);
+    imageSelect.setColour(juce::TextButton::textColourOnId, colours["btnOnTxt"]);
+    imageSelect.setColour(juce::TextButton::buttonColourId, colours["labelBg"]);
+    imageSelect.setColour(juce::TextButton::buttonOnColourId, colours["btnOnBg"]);
+    imageSelect.setButtonText("Image");
+
+    uploadBtn.setColour(juce::TextButton::textColourOffId, colours["font"]);
+    uploadBtn.setColour(juce::TextButton::textColourOnId, colours["btnOnTxt"]);
+    uploadBtn.setColour(juce::TextButton::buttonColourId, colours["labelBg"]);
+    uploadBtn.setColour(juce::TextButton::buttonOnColourId, colours["btnOnBg"]);
+    uploadBtn.setButtonText("Upload");
+
+    output.setColour(juce::Label::backgroundColourId, colours["bg"]);
+    output.setJustificationType(juce::Justification::centred);
+    output.setFont(boxFont);
 }
 
 void BeatUploaderAudioProcessorEditor::resized()
 {
+    // places gui elements on the screen
+    int marginX = 16;
+    int marginY = 10;
+    int spacing = 18;
+    int cursor = marginY; // points to current place on Y axis to place next label
 
+    title.setBounds(marginX, cursor, screenWidth - (2 * marginX), 42);
+    cursor += (42 + spacing);
+
+    loginBtn.setBounds(marginX, cursor, 110, 20);
+    info.setBounds(marginX + 110 + spacing, cursor, screenWidth - ((2 * marginX) + 110 + spacing), 19);
+    cursor += (20 + (4 * spacing));
+
+    titleBox.setBounds(marginX, cursor, 110, 23);
+    cursor += (23 + spacing);
+
+    dscBox.setBounds(marginX, cursor, screenWidth - (2 * marginX), 216);
+    cursor += (216 + spacing);
+
+    audioSelect.setBounds(marginX, cursor, 110, 20);
+    imageSelect.setBounds(screenWidth - (marginX + 110), cursor, 110, 20);
+    cursor += (20 + (2 * spacing));
+
+    uploadBtn.setBounds(screenWidth - (marginX + 110), cursor, 110, 20);
+    
+    output.setBounds(marginX, screenHeight - (marginY + 23), screenWidth - (2 * marginX), 23);
 }
--- BeatUploader/Source/PluginEditor.h
+++ BeatUploader/Source/PluginEditor.h
@@ -2,6 +2,22 @@
 
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
+#include <map>
+
+// custom font for text buttons
+class customTextButtonFont : public juce::LookAndFeel_V4
+{
+public:
+    customTextButtonFont(const juce::Font& f) : customFont(f) {}
+
+    juce::Font getTextButtonFont(juce::TextButton&, int buttonHeight) override
+    {
+        return customFont;
+    }
+
+private:
+    juce::Font customFont;
+};
 
 class BeatUploaderAudioProcessorEditor  : public juce::AudioProcessorEditor
 {
@@ -15,8 +31,25 @@ public:
 private:
     BeatUploaderAudioProcessor& audioProcessor;
 
+    void upload();
+    void login();
+
+    // colours map (declared in constructor)
+    std::map<juce::String, juce::Colour> colours;
+
+    // constants
+    int screenWidth = 400;
+    int screenHeight = 650;
+    juce::String API_URL = "not avaliable yet";
+
+    // control variables
+    bool audioChosen = false;
+    bool imageChosen = false;
+    bool uploadedSuccessfully = false; // control to not auto spam, user must shut down the plugin to upload another beat after submitting one successfully
+    bool loggedIn = false;
+
     // google oauth login
-    juce::TextButton login;
+    juce::TextButton loginBtn;
 
     // textbox input
     juce::TextEditor titleBox;
@@ -25,14 +58,22 @@ private:
     // file input and upload btn
     juce::TextButton audioSelect;
     juce::TextButton imageSelect;
-    juce::TextButton upload;
+    juce::TextButton uploadBtn;
 
     // files
     juce::File audio;
     juce::File image;
 
     // labels
+    juce::Label title;
+    juce::Label info;
     juce::Label output;
+
+    // fonts
+    juce::Font titleFont, infoFont, boxFont, buttonFont;
+    juce::Typeface::Ptr titleTypeface, infoTypeface, boxTypeface, buttonTypeface;
+
+    std::unique_ptr<customTextButtonFont> lookAndFeel;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (BeatUploaderAudioProcessorEditor)
 };
